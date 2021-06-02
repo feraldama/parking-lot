c999a2c99c7e10e7702ac99d2260903fa0eddcfd
Fernando Aldama <30935326+feraldama@users.noreply.github.com>
2021-06-02 16:58:01 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -19,6 +19,6 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
 // Syncing all the models at once.
-server.listen(3001, () => {
+server.listen(process.env.PORT, () => {
   console.log("%s listening at 3001"); // eslint-disable-line no-console
 });
